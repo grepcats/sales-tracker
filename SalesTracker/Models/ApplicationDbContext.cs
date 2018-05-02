9e244d80c58f21e4673c41005b7f09e548554745
Kayla Ondracek and Johnny Mayer <kayla.renee@gmail.com and johnnymayer@gmail.com>
2018-05-02 09:27:57 -0700

    add authentication tables and product class/table

---
+++ SalesTracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesTracker.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        //protected override void OnModelCreating(ModelBuilder builder)
+        //{
+        //    base.OnModelCreating(builder);
+        //}
+    }
+}
---
+++ SalesTracker/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SalesTracker.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public int InventoryTotal { get; set; }
+        public decimal WholesalePrice { get; set; }
+        public decimal SalePrice { get; set; }
+
+        public void setSalePrice()
+        {
+            SalePrice = WholesalePrice * (decimal)2.5;
+        }
+    }
+}
